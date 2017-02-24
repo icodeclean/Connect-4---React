2af355b8da97d3c2743aae5ed1d4b569e3d009d2
Kb Bishi <icodeclean@gmail.com>
2017-02-24 08:58:24 -0500

    Turn based play draft

--- connect4.rb
+++ connect4.rb
@@ -7,6 +7,8 @@ class Connect4
 
   @p1 = 1
   @p2 = 2
+  @current_player_is_p1 = true
+
 
 def self.show_board(array)
   puts 
@@ -30,19 +32,47 @@ def self.insert_player_coin(column, player)
   end
 end
 
+def self.toggle_player
+  @current_player_is_p1 = !@current_player_is_p1
+end
+
+def self.play_turn(column, player)
+  insert_player_coin(column, player)
+  toggle_player
+end
+
+def self.game_play(column)
+  show_board(@board)
+  i = 0
+  column = 0
+  while (i < 9) do
+  #get user input (column)
+    if @current_player_is_p1
+      play_turn(column, @p1)
+    else
+      play_turn(column, @p2)
+    end
+    i += 1
+    if column <2
+      column += 1
+    else
+      column = 0
+    end
+  end
+end
 
 
-
-show_board(@board)
-insert_player_coin(2,@p1)
-insert_player_coin(0,@p2)
-insert_player_coin(0,@p1)
-insert_player_coin(0,@p2)
-insert_player_coin(1,@p1)
-insert_player_coin(2,@p2)
-insert_player_coin(1,@p1)
-insert_player_coin(2,@p2)
-insert_player_coin(1,@p1)
+game_play(0)
+# show_board(@board)
+# play_turn(2, @p1)
+# play_turn(0, @p2)
+# play_turn(0,@p1)
+# play_turn(0,@p2)
+# play_turn(1,@p1)
+# play_turn(2,@p2)
+# play_turn(1,@p1)
+# play_turn(2,@p2)
+# play_turn(1,@p1)
 
 
 end
