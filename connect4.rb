0419700dcd07549150b16c4a6d94b1c95d919a55
Kb Bishi <icodeclean@gmail.com>
2017-02-24 12:10:57 -0500

    Randomized Computer Player Implemented

--- connect4.rb
+++ connect4.rb
@@ -1,8 +1,8 @@
 class Connect4
   @board = [
-    [nil, nil, nil],
-    [nil, nil, nil],
-    [nil, nil, nil]
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
   ]
 
   @p1 = 1
@@ -12,7 +12,7 @@ class Connect4
 
 def self.show_board(array)
   puts 
-  puts 'This is the Board'
+  puts 'Connect 4'
   puts '*' * 10
   array.each do |row|
     print row
@@ -26,16 +26,12 @@ def self.prompt
   print "> "
 end
 
+
 def self.get_player_input()
-  if @current_player_is_p1
-    player = 'Player 1'
-  else
-    player = 'Player 2'
-  end
-  puts "#{player}, what column would you like to play?"
+  puts "Player 1, what column would you like to play?"
   prompt
   column_picked = gets.chomp.to_i
-  if (@board[0][column_picked]).nil?
+  if (@board[0][column_picked] == 0)
     return column_picked
   else
     puts "That Column is Full! Please Select Another One"
@@ -44,51 +40,64 @@ def self.get_player_input()
   end
 end
 
+
 def self.insert_player_coin(column, player)
   for row in @board.reverse do  #iterate starting from bottom row
-    if row[column].nil?
-      row[column]=player
+    if row[column] == 0
+      row[column] = player
       show_board(@board)
       break
     end
   end
 end
 
+
 def self.toggle_player
   @current_player_is_p1 = !@current_player_is_p1
 end
+
 
 def self.play_turn(column, player) 
     insert_player_coin(column, player)
     toggle_player
 end
 
+def self.computer_play
+  column_picked = 1
+  available_cells = []
+  for row in @board.reverse do
+   if row.include?(0)
+      row.each_with_index do |x , index|
+        if x == 0
+          available_cells << index
+          puts 'available: ' 
+          p available_cells.join(',')
+          column_picked = available_cells.sample
+          puts 'picked: ' + column_picked.to_s
+        end
+      end
+    end
+    break
+  end
+  play_turn(column_picked, @p2) 
+end
+
+
 def self.game_play
   show_board(@board)
   i = 0
   while (i < 9) do
-  column = get_player_input
     if @current_player_is_p1
+      column = get_player_input
       play_turn(column, @p1)
     else
-      play_turn(column, @p2)
+      computer_play
     end
     i += 1
   end
 end
 
 game_play()
-# show_board(@board)
-# play_turn(2, @p1)
-# play_turn(0, @p2)
-# play_turn(0,@p1)
-# play_turn(0,@p2)
-# play_turn(1,@p1)
-# play_turn(2,@p2)
-# play_turn(1,@p1)
-# play_turn(2,@p2)
-# play_turn(1,@p1)
-
 
 end
 
