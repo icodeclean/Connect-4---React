974551a8e35a0e7aae9a263cd1b43e62f9e50bc7
Kb Bishi <icodeclean@gmail.com>
2017-02-27 14:33:30 -0500

    Code Clean up

--- js/connect4.js
+++ js/connect4.js
@@ -5,12 +5,12 @@ class Cell extends React.Component {
     } else if (cellValue == 'y') {
       return 'yellowCoin'
     } else {
-      return 
+      return;
     }
   }
   render() {
     return (
-      <div className={"cell " + this.cellClass(this.props.cellValue)} onClick={() => this.props.onClick()}></div>
+      <div className={"cell " + (this.cellClass(this.props.cellValue) || 'empty')} onClick={() => this.props.onClick()}></div>
     );
   }
 };
@@ -40,8 +40,8 @@ class Board extends React.Component {
 
   winningString(string) {
     if (string.includes('rrrr') || string.includes('yyyy')) {
-          return true;
-      }
+      return true;
+    }
   }
 
   calculateWinner(grid) {
@@ -60,7 +60,7 @@ class Board extends React.Component {
         column += grid[r][c];
       }
       if (this.winningString(column)) {
-          return true;
+        return true;
       }
     }
     return false;
@@ -78,7 +78,7 @@ class Board extends React.Component {
   }
 
   winDiagonal(grid) {
-    
+
     var diagonal1 = '';
     for (var r = 2; r < 6 ; r ++) {
       diagonal1 += grid[r][r-2];
@@ -138,23 +138,23 @@ class Board extends React.Component {
     for (var r = 5, c = 3; r > 1 ; r--, c++) {
       diagonal12 += grid[r][c];
     }
-  
-    if ((this.winningString(diagonal1))   ||
-        (this.winningString(diagonal2))   ||
-        (this.winningString(diagonal3))   ||
-        (this.winningString(diagonal4))   ||
-        (this.winningString(diagonal5))   ||
-        (this.winningString(diagonal6))   ||
-        (this.winningString(diagonal7))   ||
-        (this.winningString(diagonal8))   ||
-        (this.winningString(diagonal9))   ||
-        (this.winningString(diagonal10))  ||
-        (this.winningString(diagonal11))  ||
-        (this.winningString(diagonal12)))  {
-      return true;
-      }
-    return false;
+
+    if ((this.winningString(diagonal1))  ||
+       (this.winningString(diagonal2))   ||
+       (this.winningString(diagonal3))   ||
+       (this.winningString(diagonal4))   ||
+       (this.winningString(diagonal5))   ||
+       (this.winningString(diagonal6))   ||
+       (this.winningString(diagonal7))   ||
+       (this.winningString(diagonal8))   ||
+       (this.winningString(diagonal9))   ||
+       (this.winningString(diagonal10))  ||
+       (this.winningString(diagonal11))  ||
+       (this.winningString(diagonal12)))  {
+       return true;
   }
+  return false;
+}
 
 dropInColumn(col) {
   const grid = this.state.grid.slice();
@@ -183,7 +183,7 @@ computerPlay(){
     }
   });
   var comPick = Math.floor(Math.random() * availableCells.length);
-    return availableCells[comPick];
+  return availableCells[comPick];
 }
 
 toggleAI() {
@@ -193,96 +193,96 @@ toggleAI() {
 renderCell(col, row) {
   return <Cell cellValue={this.state.grid[row][col]} onClick={() => this.dropInColumn(col)}/>
 }
-  render() {
-    const winner = this.calculateWinner(this.state.grid);
-    let status;
-    let statusColor;
-    if (winner) {
-      status = 'Winner is ' + (!(this.state.redIsNext) ? "Red!" : "Yellow!");
-      statusColor = (!(this.state.redIsNext) ? 'r' : 'y');
-    } else {
-      status = (this.state.redIsNext ? "Red's Turn" : "Yellow's Turn");
-      statusColor = (this.state.redIsNext ? 'r' : 'y');
-    }
 
-
-    return (
-      <div className="board">
-        <div className="grid">
-          <div className="column">
-            {this.renderCell(0, 0)}
-            {this.renderCell(0, 1)}
-            {this.renderCell(0, 2)}
-            {this.renderCell(0, 3)}
-            {this.renderCell(0, 4)}
-            {this.renderCell(0, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(1, 0)}
-            {this.renderCell(1, 1)}
-            {this.renderCell(1, 2)}
-            {this.renderCell(1, 3)}
-            {this.renderCell(1, 4)}
-            {this.renderCell(1, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(2, 0)}
-            {this.renderCell(2, 1)}
-            {this.renderCell(2, 2)}
-            {this.renderCell(2, 3)}
-            {this.renderCell(2, 4)}
-            {this.renderCell(2, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(3, 0)}
-            {this.renderCell(3, 1)}
-            {this.renderCell(3, 2)}
-            {this.renderCell(3, 3)}
-            {this.renderCell(3, 4)}
-            {this.renderCell(3, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(4, 0)}
-            {this.renderCell(4, 1)}
-            {this.renderCell(4, 2)}
-            {this.renderCell(4, 3)}
-            {this.renderCell(4, 4)}
-            {this.renderCell(4, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(5, 0)}
-            {this.renderCell(5, 1)}
-            {this.renderCell(5, 2)}
-            {this.renderCell(5, 3)}
-            {this.renderCell(5, 4)}
-            {this.renderCell(5, 5)}
-          </div>
-
-          <div className="column">
-            {this.renderCell(6, 0)}
-            {this.renderCell(6, 1)}
-            {this.renderCell(6, 2)}
-            {this.renderCell(6, 3)}
-            {this.renderCell(6, 4)}
-            {this.renderCell(6, 5)}
-          </div>
-        </div>
-        <div className="dashboard">
-          <button className="button" onClick={() => this.toggleAI()}>{this.state.AI ? 'Play vs Human' : 'Play vs Computer'}</button>
-          <div className={"status "+ statusColor}>{status}</div>
-          </div>
-      </div>
-    );
+render() {
+  const winner = this.calculateWinner(this.state.grid);
+  let status;
+  let statusColor;
+  if (winner) {
+    status = 'Winner is ' + (!(this.state.redIsNext) ? "Red!" : "Yellow!");
+    statusColor = (!(this.state.redIsNext) ? 'r' : 'y');
+  } else {
+    status = (this.state.redIsNext ? "Red's Turn" : "Yellow's Turn");
+    statusColor = (this.state.redIsNext ? 'r' : 'y');
   }
+
+  return (
+    <div className="board">
+      <div className="grid">
+        <div className="column">
+          {this.renderCell(0, 0)}
+          {this.renderCell(0, 1)}
+          {this.renderCell(0, 2)}
+          {this.renderCell(0, 3)}
+          {this.renderCell(0, 4)}
+          {this.renderCell(0, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(1, 0)}
+          {this.renderCell(1, 1)}
+          {this.renderCell(1, 2)}
+          {this.renderCell(1, 3)}
+          {this.renderCell(1, 4)}
+          {this.renderCell(1, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(2, 0)}
+          {this.renderCell(2, 1)}
+          {this.renderCell(2, 2)}
+          {this.renderCell(2, 3)}
+          {this.renderCell(2, 4)}
+          {this.renderCell(2, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(3, 0)}
+          {this.renderCell(3, 1)}
+          {this.renderCell(3, 2)}
+          {this.renderCell(3, 3)}
+          {this.renderCell(3, 4)}
+          {this.renderCell(3, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(4, 0)}
+          {this.renderCell(4, 1)}
+          {this.renderCell(4, 2)}
+          {this.renderCell(4, 3)}
+          {this.renderCell(4, 4)}
+          {this.renderCell(4, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(5, 0)}
+          {this.renderCell(5, 1)}
+          {this.renderCell(5, 2)}
+          {this.renderCell(5, 3)}
+          {this.renderCell(5, 4)}
+          {this.renderCell(5, 5)}
+        </div>
+
+        <div className="column">
+          {this.renderCell(6, 0)}
+          {this.renderCell(6, 1)}
+          {this.renderCell(6, 2)}
+          {this.renderCell(6, 3)}
+          {this.renderCell(6, 4)}
+          {this.renderCell(6, 5)}
+        </div>
+      </div>
+      <div className="dashboard">
+        <button className="button" onClick={() => this.toggleAI()}>{this.state.AI ? 'Play vs Human' : 'Play vs Computer'}</button>
+        <div className={"status "+ statusColor}>{status}</div>
+      </div>
+    </div>
+    );
+}
 };
 // =======================================
 
 ReactDOM.render(
   <Board cellValue="null"/>,
   document.getElementById('container')
-);
+  );
